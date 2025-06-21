e8d21d2824322345efdb8d8af2a336da484eedf4
bmpq <73074311+bmpq@users.noreply.github.com>
2025-06-21 08:32:03 +0300

    init

---
+++ Plugin.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace tarkin.bundlesceneplayer
+{
+    [BepInPlugin("com.tarkin.bundlesceneplayer", MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static new ManualLogSource Log;
+
+        private void Awake()
+        {
+            Log = base.Logger;
+
+            InitConfiguration();
+        }
+
+        private void InitConfiguration()
+        {
+        }
+    }
+}
