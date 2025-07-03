f7ea83746909c30c53b0856f7b43610a5604ef34
bmpq <73074311+bmpq@users.noreply.github.com>
2025-07-03 06:28:01 +0300

    more hierarchy manipulations

---
+++ BSP.Shared/HierarchyChild.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace tarkin.BSP.Shared
+{
+    [DefaultExecutionOrder(10)]
+    internal class HierarchyChild : MonoBehaviour
+    {
+        public string path;
+
+        void Start()
+        {
+            GameObject parent = GameObject.Find(path);
+            if (parent != null)
+                transform.SetParent(parent.transform, true);
+        }
+    }
+}
---
+++ BSP.Shared/HierarchyLocalTransformModifier.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace tarkin.BSP.Shared
+{
+    internal class HierarchyLocalTransformModifier : MonoBehaviour
+    {
+        public string path;
+
+        private Transform target;
+        private Vector3 initPos;
+        private Quaternion initRot;
+        private Vector3 initScale;
+
+        void OnEnable()
+        {
+            target = GameObject.Find(path)?.transform;
+            if (target != null)
+            {
+                initPos = target.localPosition;
+                initRot = target.localRotation;
+                initScale = target.localScale;
+
+                target.localPosition = transform.localPosition;
+                target.localRotation = transform.localRotation;
+                target.localScale = transform.localScale;
+            }
+        }
+
+        void OnDisable()
+        {
+            if (target != null)
+            {
+                target.localPosition = initPos;
+                target.localRotation = initRot;
+                target.localScale = initScale;
+            }
+        }
+    }
+}
