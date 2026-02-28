215086492f0e62edb5652e50d14e378b429d3ecf
bmpq <73074311+bmpq@users.noreply.github.com>
2026-02-28 10:00:22 +0300

    Create PlayerForceReact.cs

---
+++ TarkinDirector/PlayerForceReact.cs
@@ -0,0 +1,27 @@
+﻿using Comfort.Common;
+using EFT;
+using UnityEngine;
+
+namespace tarkin.Director
+{
+    internal class PlayerForceReact : MonoBehaviour
+    {
+        public bool applyOnEnable = true;
+        public float strength = 1f;
+        public float recoilHands = 2f;
+        public float recoilCamera = 4f;
+
+        void OnEnable()
+        {
+            if (applyOnEnable)
+                ApplyForceToMainPlayer();
+        }
+
+        public void ApplyForceToMainPlayer()
+        {
+#if EFT_RUNTIME
+            Singleton<GameWorld>.Instance.MainPlayer.ProceduralWeaponAnimation.ForceReact.AddForce(strength, recoilHands, recoilCamera);
+#endif
+        }
+    }
+}
